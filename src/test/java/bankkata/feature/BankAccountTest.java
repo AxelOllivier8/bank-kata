9329ffb8225de5332979ed1d133935c4fa46977e
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-24 11:58:39 +0100

    First commit with always true test

---
+++ src/test/java/bankkata/feature/BankAccountTest.java
@@ -0,0 +1,15 @@
+package bankkata.feature;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankAccountTest {
+
+    @Test
+    public void firstTest(){
+        assertThat(false).isFalse();
+    }
+
+
+}
